d2428ebdcb4cdfc6b7fdcd24145ffd0cac6c58ef
ryannewhouse11 <crnewhouse22@gmail.com>
2026-09-15 08:43:16 -0400

    more frontend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,19 +11,27 @@ type AppUser = {
   email: string
 }
 
+type Team = {
+  id: string
+  name: string
+  createdAt: string
+}
+
 function App() {
   
   const [session, setSession] = useState<Session | null>(null)
   const [appUser, setAppUser] = useState<AppUser | null>(null)
   const [loading, setLoading] = useState(true)
+  const [teams, setTeams] = useState<Team[]>([])
 
   useEffect(() => {
     const loadUser = async (currentSession: Session | null) =>  {
       
       setSession(currentSession)
+      setLoading(true)
 
       if(!currentSession) {
-        
+        setTeams([])
         setAppUser(null)
         setLoading(false)
         return
@@ -32,18 +40,34 @@ function App() {
         
       try {
        
-        const response = await fetch(
+        const userResponse = await fetch(
           `http://localhost:8080/api/users/auth/${currentSession.user.id}`
         )
 
         
-        if (!response.ok) {
-          throw new Error(`Backed returned ${response.status}`)
+        if (!userResponse.ok) {
+          throw new Error(`Backed returned ${userResponse.status}`)
         }
 
-        const user: AppUser = await response.json()
+        const user: AppUser = await userResponse.json()
        
         setAppUser(user)
+        try {
+        const teamsResponse = await fetch(
+          `http://localhost:8080/api/user/teams/${user.id}`
+        )
+
+        if(!teamsResponse.ok) {
+          throw new Error(`Backend returned ${teamsResponse.status}`)
+        }
+
+        const userTeams: Team[] = await teamsResponse.json()
+        setTeams(userTeams)
+      } catch (error) {
+        console.error('Error loading teams', error)
+        setTeams([])
+      }
+
       } catch (error) {
         console.error('Error loading application user:', error)
         setAppUser(null)
@@ -79,11 +103,23 @@ function App() {
     return <p>Unable to load your application account.</p>
   }
 
+
   return (
     <div>
       <h1>Welcolme, {appUser?.name}</h1>
       <p>{appUser.email}</p>
       <p>{session.user.id}</p>
+      <h2>Your Teams</h2>
+
+      {teams.length === 0 ? (
+        <p>You are not currently on a team.</p>
+      ) : (
+        <ul>
+          {teams.map((team) => (
+            <li key={team.id}>{team.name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   )
 }
